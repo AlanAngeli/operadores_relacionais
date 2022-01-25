54232c183257844913fc74c4ecfcbde743d80d4a
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-01-25 12:23:59 -0300

    Add files via upload

---
+++ operadores_relacionais.py
@@ -0,0 +1,32 @@
+""""
+Operadores relacionais
+
+== igualdade
+> maior
+>= maior ou igual
+< menor
+<= menor ou igual
+!= diferente
+
+"""
+
+#ex:
+
+numero_1 = 7
+numero_2 = 7
+
+print(numero_1 == numero_2) # O resultado será True
+
+print(numero_1 > numero_2) # O resultado será False, pois os números são iguais
+
+# Exemplo com variáveis:
+
+var_1 = 'João'
+var_2 = 'Maria'
+
+print(var_1 != var_2) # O resultado é True, pois são diferentes
+print(var_1 == var_2) # O resultado é False, pois não são iguais
+
+
+
+
